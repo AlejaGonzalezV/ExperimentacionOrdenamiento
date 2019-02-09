f4968530e01589cb7b40362299d5e5a91b714e3f
angieVa <angievalentina1526@gmail.com>
2019-02-09 16:48:57 -0500

    metodo insertion sort y variable tiempo

--- ExpOrdenamiento/ExpOrdenamiento/Algoritmo.cs
+++ ExpOrdenamiento/ExpOrdenamiento/Algoritmo.cs
@@ -11,18 +11,28 @@ namespace ExpOrdenamiento
 
         private int[] arreglo1;
         private int[] arreglo2;
+        private double tiempo;
 
-    
+
         public Algoritmo()
         {
 
             arreglo1 = new int[100];
             arreglo2 = new int[1000000];
+            tiempo = 0;
 
         }
 
+
+        public int[] Arreglo1 { get => arreglo1; set => arreglo1 = value; }
+        public int[] Arreglo2 { get => arreglo2; set => arreglo2 = value; }
+        public double Tiempo { get => tiempo; set => tiempo = value; }
+
+
         public void shellSort()
         {
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
 
             int salto = 0;
             int sw = 0;
@@ -51,7 +61,33 @@ namespace ExpOrdenamiento
                 salto = salto / 2;
             }
 
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            tiempo = stop.Subtract(start).TotalMilliseconds;
         }
+
+        public void insertionSort(double[] arrayToSort)
+        {
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
+
+            for (int i = 1; i < arrayToSort.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    double current = arrayToSort[j];
+                    double toEvaluate = arrayToSort[j - 1];
+                    if (toEvaluate > current)
+                    {
+                        double aux = arrayToSort[j - 1];
+                        arrayToSort[j - 1] = arrayToSort[j];
+                        arrayToSort[j] = aux;
+                    }
+                }
+            }
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            tiempo = stop.Subtract(start).TotalMilliseconds;
+        }
+
 
     }
 }
